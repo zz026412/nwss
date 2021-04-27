a0672710a56331e9f274257b7da2856142ad00e6
hancush <hannah.cushman@gmail.com>
2021-04-27 11:32:03 -0500

    Add first draft

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,34 @@
-const jsonschema = require('jsonschema');
+const xlsx = require('xlsx');
+const validate = require('jsonschema').validate;
+const schema = require('./schema.json');
 
-console.log(jsonschema);
+const uploadForm = document.getElementById('upload-form');
+var outputDiv = document.getElementById('output');
 
-console.log('hello world!');
+uploadForm.addEventListener('change', (event) => {
+    outputDiv.innerHTML = '';
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        var data = new Uint8Array(e.target.result);
+        var workbook = xlsx.read(data, {type: 'array'});
+        // TODO: Show prompt allowing user to pick sheet
+        var firstSheet = Object.keys(workbook.Sheets)[0];
+        var arrayData = xlsx.utils.sheet_to_json(workbook.Sheets[firstSheet]);
+
+        var result = validate(arrayData, schema);
+        if ( result.errors.length > 0 ) {
+            result.errors.forEach(error => {
+                outputDiv.insertAdjacentHTML(
+                    'beforeend',
+                    `Error in record ${error.path[0]}: ${error.message}<br />`
+                );
+            });
+        } else {
+            output.innerHTML = 'Upload is valid!';
+        };
+    };
+
+    reader.readAsArrayBuffer(file);
+});
