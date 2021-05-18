1dfe07cd08d5d24c258be197c943b72c6ccfdec8
Sam McAlilly <sammcalilly@gmail.com>
2021-05-18 11:35:13 -0500

    re-approach time bug

--- nwss/dump_to_jsonschema.py
+++ nwss/dump_to_jsonschema.py
@@ -212,10 +212,18 @@ for key, property in properties.items():
         if 'null' in property['type']:
             property['enum'].append(None)
 
+# Add a regex to validate the time string based on the pattern.
+hh_mm_ss_regex = '^([0-1]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$'
+properties['sample_collect_time']['pattern'] = hh_mm_ss_regex
+# 'format' = 'time' for this field. Remove it so the regex validates instead.
+properties['sample_collect_time'].pop('format')
+
 s['definitions']['WaterSampleSchema'].update({
     'properties': {**properties},
     **custom_validators
 })
+
+
 
 # Reshape the schema so it accepts an array
 # of the WaterSampleSchema objects.
--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -81,6 +81,18 @@ def test_valid_json_schema(valid_json, json_schema):
             },
             pytest.raises(jsonschema.ValidationError)
         ),
+        (
+            {
+                'sample_collect_time': '25:58:00' # this time doesn't exist
+            },
+            pytest.raises(jsonschema.ValidationError)
+        ),
+        (
+            {
+                'sample_collect_time': 'words not a time'
+            },
+            pytest.raises(jsonschema.ValidationError)
+        )
     ]
 )
 def test_invalid_json_schema(valid_json, json_schema, input, expect):
