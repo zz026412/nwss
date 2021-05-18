b950ba5c576b0956ce72b731a4865d95c0328cda
Sam McAlilly <sammcalilly@gmail.com>
2021-05-18 11:47:25 -0500

    remove dead code

--- docs/js/main.js
+++ docs/js/main.js
@@ -76,11 +76,6 @@ class FileValidator {
             validate: data => Number.isInteger(data)
         })
 
-        ajv.addFormat('time', {
-            type: 'string',
-            validate: data => new RegExp('^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$').test(data)
-        })
-
         ajv.addKeyword({
             keyword: 'units'
         })
