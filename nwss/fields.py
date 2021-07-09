295bf72df8a602fa617b5dc37984f0cf507c416d
hancush <hannah.cushman@gmail.com>
2021-07-09 11:38:45 -0500

    Include allowed values if categorical data are missing

--- nwss/fields.py
+++ nwss/fields.py
@@ -1,5 +1,30 @@
 from marshmallow import fields
 
+from nwss import validators as nwss_validators
+
+
+class CategoricalString(fields.String):
+
+    def __init__(self, *args, **kwargs):
+        try:
+            allowed_values = kwargs.pop('allowed_values')
+        except KeyError:
+            raise TypeError("Missing required keyword argument 'allowed_values'")
+
+        # Add allowed value validation
+        kwargs['validate'] = nwss_validators.CaseInsensitiveOneOf(allowed_values)
+
+        # Get error_messages, if provided, or create a fresh dict
+        error_messages = kwargs.pop('error_messages', {})
+
+        # Included allowed values if data missing
+        error_messages['required'] = ('Missing data for required field. '
+                                      f'Expected one of: {", ".join(allowed_values)}')
+        kwargs['error_messages'] = error_messages
+
+        # Initialize as normal
+        super().__init__(*args, **kwargs)
+
 
 class ListString(fields.String):
     '''
